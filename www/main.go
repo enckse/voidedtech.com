b3df5812023a28a474bf5c68bec72afb401bb6b2
Sean Enck <enckse@voidedtech.com>
2022-09-18 10:22:50 -0400

    merging files down

--- www/main.go
+++ www/main.go
@@ -20,6 +20,27 @@ var (
 )
 
 type (
+	// SiteData is the necessary data to render the site.
+	SiteData struct {
+		Date     string
+		IsPublic bool
+		Title    string
+		Header   string
+		Links    []Link
+		Mail     string
+		Places   []Place
+	}
+	// Link represents page links.
+	Link struct {
+		Href    string
+		Display string
+	}
+	// Place indicate a place I have worked/learned at.
+	Place struct {
+		Name string
+		From string
+		To   string
+	}
 	hostData struct {
 		mainIndex []byte
 		mainCSS   []byte
