fc2941b28d7d6834f56ab27f3bf3cca30f09f935
Sean Enck <enckse@voidedtech.com>
2022-11-04 09:48:39 -0400

    IsPublic is deprecated

--- www/main.go
+++ www/main.go
@@ -23,13 +23,12 @@ var (
 type (
 	// SiteData is the necessary data to render the site.
 	SiteData struct {
-		Date     string
-		IsPublic bool
-		Title    string
-		Header   string
-		Links    []Link
-		Mail     string
-		Places   []Place
+		Date   string
+		Title  string
+		Header string
+		Links  []Link
+		Mail   string
+		Places []Place
 	}
 	// Link represents page links.
 	Link struct {
